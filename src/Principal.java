2d96f171b41e4df3a994572bcd709eb9159bfef2
Daniel Acosta <adanispro@gmail.com>
2024-12-27 08:06:02 -0500

    Creacion

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,31 @@
+import com.google.gson.Gson;
+
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+
+        Scanner teclado=new Scanner(System.in);
+
+        System.out.println("*************************************");
+        System.out.println("Bienvenido/a al conversor de moneda :)");
+        System.out.println("");
+        System.out.println("1) Dólar =>> Peso argentino");
+        System.out.println("2) Peso argentino =>> Dólar");
+        System.out.println("3) Dólar =>> Real brasileño");
+        System.out.println("4) Real brasileño =>> Dólar");
+        System.out.println("5) Dólar =>> Peso colombiano");
+        System.out.println("6) Peso colombiano =>> Dólar");
+        System.out.println("7) Salir");
+        System.out.println("");
+        System.out.println("Elija una opción válida: ");
+        System.out.println("hola xd");
+
+
+
+
+
+
+
     }
 }
